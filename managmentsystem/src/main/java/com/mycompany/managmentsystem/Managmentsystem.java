6876b34bfb5306e485906ffe5b8c3cd47063d1cf
rayaodeh <86550999+rayaodeh@users.noreply.github.com>
2023-05-14 22:21:00 +0200

    java and Sql project

---
+++ managmentsystem/src/main/java/com/mycompany/managmentsystem/Managmentsystem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.managmentsystem;
+
+/**
+ *
+ * @author RayaOdeh
+ */
+public class Managmentsystem {
+
+    public static void main(String[] args) {
+EmployeeInfo ms=new EmployeeInfo();
+ms.setVisible(true);
+    }
+}
